195ef657393b6fee5880affa330e89b3c34b265c
Jose Maria <josmariaguilar@gmail.com>
2022-10-16 08:22:53 -0400

    Refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
 import { Content } from './components/Content';
 
-import './App.css';
-
 export const App = () => {
   return (
-    <div className='App'>
-      <Content />
-    </div>
+    <Content />
   );
 }
